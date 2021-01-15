64bc6edb7bd98523723e86bbf90cc6fba39f7164
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2021-01-14 23:41:45 -0500

    remove incompatible server warning when not present

--- src/main/java/net/permutated/flickerfix/FlickerFix.java
+++ src/main/java/net/permutated/flickerfix/FlickerFix.java
@@ -1,7 +1,11 @@
 package net.permutated.flickerfix;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,5 +24,8 @@ public class FlickerFix {
     public FlickerFix() {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+            () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
