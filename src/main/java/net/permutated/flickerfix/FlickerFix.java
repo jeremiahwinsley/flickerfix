8942cf5986a86e9a5c4be4b659805dca1ea56036
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2021-12-01 18:17:04 -0500

    Update to 1.18

--- src/main/java/net/permutated/flickerfix/FlickerFix.java
+++ src/main/java/net/permutated/flickerfix/FlickerFix.java
@@ -1,11 +1,9 @@
 package net.permutated.flickerfix;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.network.NetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,11 +19,9 @@ public class FlickerFix {
     public static final float brightness = 1.0F;
     public static final float fadeRate = brightness / fadeTicks;
 
+    @SuppressWarnings("java:S1118")
     public FlickerFix() {
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
-            () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+            () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
     }
 }
--- src/main/java/net/permutated/flickerfix/mixin/MixinGameRenderer.java
+++ src/main/java/net/permutated/flickerfix/mixin/MixinGameRenderer.java
@@ -2,21 +2,22 @@ package net.permutated.flickerfix.mixin;
 
 import net.permutated.flickerfix.FlickerFix;
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 import java.util.Optional;
 
+@SuppressWarnings({"java:S1118", "java:S1610"})
 @Mixin(GameRenderer.class)
 public abstract class MixinGameRenderer {
 
     @Overwrite
     public static float getNightVisionScale(LivingEntity livingEntityIn, float entitylivingbaseIn) {
-        int i = Optional.ofNullable(livingEntityIn.getEffect(Effects.NIGHT_VISION))
-            .map(EffectInstance::getDuration).orElse(0);
+        int i = Optional.ofNullable(livingEntityIn.getEffect(MobEffects.NIGHT_VISION))
+            .map(MobEffectInstance::getDuration).orElse(0);
         return i > FlickerFix.fadeTicks ? FlickerFix.brightness : i * FlickerFix.fadeRate;
     }
 }
