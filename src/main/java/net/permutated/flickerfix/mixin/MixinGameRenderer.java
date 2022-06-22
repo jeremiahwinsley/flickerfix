f18efc5e8d5483337f9fe38b6b561fa150c5cf07
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2022-06-21 22:40:04 -0400

    Update to 1.19

--- src/main/java/net/permutated/flickerfix/mixin/MixinGameRenderer.java
+++ src/main/java/net/permutated/flickerfix/mixin/MixinGameRenderer.java
@@ -6,7 +6,9 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
@@ -14,10 +16,11 @@ import java.util.Optional;
 @Mixin(GameRenderer.class)
 public abstract class MixinGameRenderer {
 
-    @Overwrite
-    public static float getNightVisionScale(LivingEntity livingEntityIn, float entitylivingbaseIn) {
+    @Inject(method = "getNightVisionScale", at = @At("HEAD"), cancellable = true)
+    private static void getNightVisionScale(LivingEntity livingEntityIn, float entitylivingbaseIn, CallbackInfoReturnable<Float> cir) {
         int i = Optional.ofNullable(livingEntityIn.getEffect(MobEffects.NIGHT_VISION))
             .map(MobEffectInstance::getDuration).orElse(0);
-        return i > FlickerFix.fadeTicks ? FlickerFix.brightness : i * FlickerFix.fadeRate;
+
+        cir.setReturnValue(i > FlickerFix.fadeTicks ? FlickerFix.brightness : i * FlickerFix.fadeRate);
     }
 }
