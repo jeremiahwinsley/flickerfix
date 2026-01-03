36ed20a3cc9b14395a00a0193c72581304619dce
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2026-01-03 11:22:26 -0500

    Update to MDG, support up to 1.21.11

--- src/main/java/net/permutated/flickerfix/FlickerFix.java
+++ src/main/java/net/permutated/flickerfix/FlickerFix.java
@@ -1,15 +1,10 @@
 package net.permutated.flickerfix;
 
 import net.neoforged.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(FlickerFix.MOD_ID)
 public class FlickerFix {
     public static final String MOD_ID = "flickerfix";
-
-    // Directly reference a log4j logger.
-    public static final Logger LOGGER = LogManager.getLogger(FlickerFix.class);
 
     //TODO move to config file
     public static final int fadeTicks = 20;
